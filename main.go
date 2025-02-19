0b4eccea97be34e3c1ed984cef734e4d1eeda876
Duong035 <a1612352210@gmail.com>
2025-02-19 21:14:29 +0700

    up

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	app2 "intern_247/app"
+	"intern_247/routes"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	app2.Setup()
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:3000",
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowCredentials: true,
+	}))
+
+	routes.AdminRoutes(app)
+
+	log.Fatal(app.Listen(":4000"))
+}
